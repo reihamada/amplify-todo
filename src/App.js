277a333e87ed665e70ff0920652ef9009e31ba4b
Rei Hamada <hamada.rei@nci.co.jp>
2022-09-08 16:48:29 +0900

    コンポーネントを開く前にユーザーのメールアドレスでログイン画面が表示されます。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from 'react';
+import Amplify, { Auth } from "aws-amplify";
+import awsmobile from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";  
+
+// Amplifyの設定を行う
+Amplify.configure(awsmobile)
+
+// SingUp時に、メールアドレスとパスワードを要求する
+const signUpConfig = {
+  header: 'Sign Up',
+  hideAllDefaults: true,
+  defaultCountyCode: '1',
+  signUpFields: [
+      {
+          label: 'User Name',
+          key: 'username',
+          required: true,
+          displayOrder: 1,
+          type: 'string'
+      },
+      {
+          label: 'Email',
+          key: 'email',
+          required: true,
+          displayOrder: 2,
+          type: 'string'
+      },
+      {
+          label: 'Password',
+          key: 'password',
+          required: true,
+          displayOrder: 3,
+          type: 'password'
+      }
+  ]
+}
+
+// SingOut
+function signOut(){
+  Auth.signOut()
+  .then()
+  .catch();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button onClick={signOut}>Sign out</button>
+      <div>Hello World</div>
+    </React.Fragment>
   );
 }
 
-export default App;
+// Appコンポーネントをラップする
+export default withAuthenticator(App,{signUpConfig});
+// export default withAuthenticator(App);
+// export default App;
